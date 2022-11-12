ec6a628bb320ceb1aa1dc8f55771a05ea400d9b0
Santiago Maza <santiagommaza@gmail.com>
2022-11-11 22:12:20 -0300

    It was added a simple commienta t Alert.js

--- src/components/Alerts/Alert.js
+++ src/components/Alerts/Alert.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './alert.css'
 
 const Alert = ({message}) => {
+  //By using the way {} instead of (props) we're destructuting props. The project will work well anyways.
   return (
     <div className="alert alert-warning" role="alert"><i className="bi bi-emoji-smile px-2"></i>{message}</div>
   )
