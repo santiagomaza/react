41a339473f3af66d6a71b8a4cd1b62243ab4556a
Santiago Maza <santiagommaza@gmail.com>
2022-11-11 21:46:43 -0300

    Alert.js props were changed from (props.message) to ({message}) and it was added the dependence Axios

--- src/components/Alerts/Alert.js
+++ src/components/Alerts/Alert.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './alert.css'
 
-const Alert = (alert) => {
+const Alert = ({message}) => {
   return (
-    <div className="alert alert-warning" role="alert"><i className="bi bi-emoji-smile px-2"></i>{alert.message}</div>
+    <div className="alert alert-warning" role="alert"><i className="bi bi-emoji-smile px-2"></i>{message}</div>
   )
 }
 
